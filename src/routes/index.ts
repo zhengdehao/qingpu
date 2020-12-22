88e9b96be931cfb44a3f1eebb261c3ffcacdbee1
Chen Tian <506445604@qq.com>
2020-12-22 15:42:12 +0800

    ct.0.3

---
+++ src/routes/index.ts
@@ -0,0 +1,15 @@
+const Home = { template: '<div>Home</div>' }
+const About = { template: '<div>About</div>' }
+
+import { createRouter, createWebHashHistory } from "vue-router"
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+]
+
+
+export const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
