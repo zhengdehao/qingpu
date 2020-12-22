7c765023f5fc0cfd7933e34a09209ab270eb3288
Chen Tian <506445604@qq.com>
2020-12-22 15:59:18 +0800

    3

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
 <template>
-    <h2>LV</h2>
     <router-link to="/"></router-link>
     <router-link to="/about"></router-link>
-    <header>
-      <ul>
-        <li v-for="(item, index) in navList" :key="index">{{ item }}</li>
-      </ul>
-    </header>
 </template>
 
 <script>
 
 export default {
   name: 'App',
-  data() {
-    return {
-      navList:["首页","分类","购物车","我的"]
-    }
-  },
   components: {
     
   }
