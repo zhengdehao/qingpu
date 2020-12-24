9c2f11180db5bc683db14211692466577320e040
lvhaha123 <670775379@qq.com>
2020-12-24 09:38:02 +0800

    4.1

--- src/views/Home/Find/index.vue
+++ src/views/Home/Find/index.vue
@@ -1,6 +1,6 @@
 <template>
-<div class="wrapper">
-  <div class="find">
+<div class="wrapper" ref="wrapper">
+  <div class="find" ref="obtain">
     <find-header v-if="local"></find-header>
     <div>
       <h1>发现</h1>
@@ -13,6 +13,7 @@
 </template>
 
 <script lang="ts">
+import BScroll from 'better-scroll';
 import FindList from "../../../components/Find/FindList.vue";
 import HumanityList from "../../../components/Find/HumanityList.vue";
 import FindHeader from "../../../components/Find/FindHeader.vue";
@@ -33,31 +34,55 @@ export default defineComponent({
   computed: {},
 
   mounted() {
-    window.addEventListener("scroll", this.handleScroll);
-  },
+    this.$nextTick(() => {
+        let BsrcollY = new BScroll(this.$refs.wrapper, {});
+        BsrcollY.on('scroll', (position) => {
+        console.log(position.x, position.y);
+        scrollTo(0, 50, , easing)
+    });
+      })
+    .addEventListener("scroll", this.handleScroll);
+    // window.addEventListener('scroll', this.handleScrollx, true)
+    // this.handleScrollx();
+    
+    }
+    
 
-  methods: {
-    handleScroll: function () {
-      // 滚动时，距离顶部的距离
-      var scrollTop =
-        document.documentElement.scrollTop || document.body.scrollTop;
-      if (scrollTop > 50) {
-        this.local = true;
-      } else {
-        this.local = false;
-      }
-    },
-  },
-  unmounted() {
-    //离开这个界面之后，删除，不然会有问题
-    window.removeEventListener("scroll", this.handleSroll);
-  },
+  // methods: {
+      
+    // handleScrollx() {
+     
+        // let scrollTop=
+        // console.log( document.getElementsByClassName("find")[0]) ;
+      //   if (scrollTop < -50) {
+      //   this.local = true;
+      // } else {
+      //   this.local = false;
+      // }
+    // },
+
+    // handleScroll: function () {
+    //   // 滚动时，距离顶部的距离
+    //   var scrollTop =
+    //     document.documentElement.scrollTop || document.body.scrollTop;
+      // if (scrollTop > 50) {
+      //   this.local = true;
+      // } else {
+      //   this.local = false;
+      // }
+    // },
+  // unmounted() {
+  //   //离开这个界面之后，删除，不然会有问题
+  //   window.removeEventListener("scroll", this.handleSroll);
+  // },
 });
 </script>
 <style lang="less" scoped>
-// .wrapper{
-  
-// }
+.wrapper{
+  height: 500px;
+  overflow: hidden;
+
+}
 .find {
   padding-top:30px;
   color: #333;
